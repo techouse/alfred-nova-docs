2abef40374112146cdb6dc4d09c494840781ad64
Klemen Tušar <techouse@gmail.com>
2019-04-28 16:17:51 +0200

    Cosmetics Reformat the code using [Black](https://github.com/ambv/black)

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ class Config(object):
     # How long to cache results for
     CACHE_MAX_AGE = 20  # seconds
     # Icon
-    NOVA_ICON = 'icon.png'
+    NOVA_ICON = "icon.png"
     # Algolia credentials
-    ALGOLIA_APP_ID = '6EVZSN4WQD'
-    ALGOLIA_SEARCH_ONLY_API_KEY = 'bf1eceffbd547a1443da21aab145c2d5'
-    ALGOLIA_SEARCH_INDEX = 'nova-docs-1.0'
+    ALGOLIA_APP_ID = "6EVZSN4WQD"
+    ALGOLIA_SEARCH_ONLY_API_KEY = "bf1eceffbd547a1443da21aab145c2d5"
+    ALGOLIA_SEARCH_INDEX = "nova-docs-1.0"
