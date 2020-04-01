a75e0f12ebf45736728580ba613d54914f3d0a57
Klemen Tušar <techouse@gmail.com>
2020-04-01 13:35:20 +0100

    :sparkles: Add support for Laravel Nova 3

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ class Config(object):
     NOVA_ICON = "icon.png"
     GOOGLE_ICON = "google.png"
     # supported docs
-    SUPPORTED_NOVA_VERSIONS = {"v2", "v1"}
-    DEFAULT_NOVA_VERSION = "2"
+    SUPPORTED_NOVA_VERSIONS = {"v3", "v2", "v1"}
+    DEFAULT_NOVA_VERSION = "3"
     # Algolia credentials
     ALGOLIA_APP_ID = "WODHKE4WZG"
     ALGOLIA_SEARCH_ONLY_API_KEY = "7456cdd91ba8d4f87846549697397759"
