9654c2b87e33b44b0d13958e943e12d63ff30146
Klemen Tušar <techouse@gmail.com>
2022-04-02 19:10:23 +0100

    :arrow_up: Updates

--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,7 +1,7 @@
 class Config {
   Config._();
 
-  static const String version = '2.0.2';
+  static const String version = '2.0.3';
   static final Uri githubRepositoryUrl =
       Uri.https('github.com', '/techouse/alfred-nova-docs');
   static const String algoliaApplicationId = 'BH4D9OD16A';
