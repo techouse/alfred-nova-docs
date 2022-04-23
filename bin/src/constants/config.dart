1d86d5c02b92ad7fb04b9863fe53fef410ea2e3d
Klemen Tušar <techouse@gmail.com>
2022-04-23 18:29:15 +0200

    :sparkles: Add support for Laravel Nova 4.0

--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,12 +1,12 @@
 class Config {
   Config._();
 
-  static const String version = '2.0.3';
+  static const String version = '2.0.4';
   static final Uri githubRepositoryUrl =
       Uri.https('github.com', '/techouse/alfred-nova-docs');
-  static const String algoliaApplicationId = 'BH4D9OD16A';
+  static const String algoliaApplicationId = 'FGRCZANQVY';
   static const String algoliaSearchOnlyApiKey =
-      '5aa44fede3f10262000a8c4f046033d5';
+      '7c0aaf326992f08ed7bfc461a1b61ef3';
   static const String algoliaSearchIndex = 'laravel_nova';
-  static const List<String> supportedVersions = ['v1', 'v2', 'v3'];
+  static const List<String> supportedVersions = ['v1', 'v2', 'v3', 'v4'];
 }
